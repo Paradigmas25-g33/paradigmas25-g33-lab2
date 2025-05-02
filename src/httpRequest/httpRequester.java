d9cb90f35357d163f91fd2cb146c12d48d6b5a73
Emilio Lobos Silhanek <emilio.lobos@mi.unc.edu.ar>
2025-05-02 16:41:56 -0300

    avances requester

--- src/httpRequest/httpRequester.java
+++ src/httpRequest/httpRequester.java
@@ -1,5 +1,10 @@
 package httpRequest;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 /* Esta clase se encarga de realizar efectivamente el pedido de feed al servidor de noticias
  * Leer sobre como hacer una http request en java
@@ -9,8 +14,28 @@ package httpRequest;
 public class httpRequester {
 	
 	public String getFeedRss(String urlFeed){
-		String feedRssXml = null;
-		return feedRssXml;
+		//String feedRssXml = null;
+		try {
+			HttpClient client = HttpClient.newHttpClient();
+
+			HttpRequest request = HttpRequest.newBuilder()
+						.uri(URI.create(urlFeed))
+						.GET()
+						.build();
+			
+						HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+						
+						if (response.statusCode() == 200) {
+							return response.body(); // XML del feed
+						} else {
+							System.err.println("Error: código de estado HTTP " + response.statusCode());
+							return null;
+						}
+		}
+		catch (Exception exc){
+			System.err.println("Excepción al obtener el feed RSS: " + exc.getMessage());
+			return null;
+		}
 	}
 
 	public String getFeedReedit(String urlFeed) {
